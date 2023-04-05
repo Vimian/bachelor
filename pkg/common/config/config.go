6bc8061efb63b521b576295bef360f1fe9d302a0
SaltIncluded <43348526+casperfj@users.noreply.github.com>
2023-04-06 01:07:02 +0200

    Added comment to config.go

--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 func LoadConfig(env string, config []byte, configuration interface{}) (interface{}, error) {
+	// unmarshal the config file
 	err := yaml.Unmarshal(config, configuration)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse YAML: %v", err)
