f001ffb6dd9beea079ad7b8fd40136b515f93112
tobber00 <39513708+tobber00@users.noreply.github.com>
2023-04-18 10:38:23 +0200

    basic frontend setup

---
+++ cmd/front-end/handlers/getBalance.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) GetBalance(c *gin.Context) {
+	// Get id from path parameter
+	id := c.Param("id")
+
+	// Get balance from repository
+	balance, err := h.repo.GetBalance(id)
+	if err != nil {
+		log.Printf("failed to get balance of account from repository. {account.id: %s}, error: %s", id, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return balance
+	c.JSON(http.StatusOK, balance)
+}
---
+++ cmd/front-end/handlers/updateBalance.go
@@ -0,0 +1,56 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	commonAccount "github.com/casperfj/bachelor/pkg/account"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) UpdateBalance(c *gin.Context) {
+	// Get id from path parameter
+	id := c.Param("id")
+
+	// Get balanceUpdate from request body
+	var balanceUpdate commonAccount.BalanceUpdate
+	if err := c.ShouldBindJSON(&balanceUpdate); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Get account from repository
+	account, err := h.repo.Get(id)
+	if err != nil {
+		log.Printf("failed to get account from repository. {account.id: %s}, error: %s", id, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Update balance
+	account.Balance = account.Balance + balanceUpdate.BalanceChange
+
+	// Check if account is overdrawn
+	if account.Balance < account.OverdrawLimit {
+		log.Printf("account is overdrawn. {account.id: %s}", id)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "account can't be overdrawn beyond overdraw limit"})
+		return
+	}
+
+	// Create new balance
+	balance := &commonAccount.Balance{
+		AccountID: account.ID,
+		Balance:   account.Balance,
+	}
+
+	// Update balance in repository
+	err = h.repo.UpdateBalance(balance)
+	if err != nil {
+		log.Printf("failed to update balance of account in repository. {account.id: %s}, error: %s", id, err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Return balance
+	c.JSON(http.StatusOK, balance)
+}
