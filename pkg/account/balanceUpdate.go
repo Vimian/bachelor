658c7715bd192c256e35b97b73b91bd3699d61fa
SaltIncluded <43348526+casperfj@users.noreply.github.com>
2023-04-23 00:03:26 +0200

    Update balanceUpdate.go

--- pkg/account/balanceUpdate.go
+++ pkg/account/balanceUpdate.go
@@ -2,5 +2,5 @@ package account
 
 type BalanceUpdate struct {
 	BalanceChange   int64 `json:"balancechange" binding:"required"`
-	TransactionType int32 `json:"transactiontype" binding:"required"`
+	TransactionType int32 `json:"transactiontype"`
 }
